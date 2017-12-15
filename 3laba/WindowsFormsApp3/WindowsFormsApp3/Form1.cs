3410b3990c1f31299aa0c9a47428d90a8399ed37
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2017-12-15 17:45:07 +0300

    Add files via upload

---
+++ 3laba/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        Color color;
+        Color dopColor;
+        int maxSpeed;
+        int maxCountEkipazh;
+        int Tonnazh;
+        int goruchee;
+        private Techno inter;
+        public Form1()
+        {
+            InitializeComponent();
+            color = Color.Red;
+            dopColor = Color.Blue;
+            maxSpeed = 10000000;
+            maxCountEkipazh = 2800;
+            Tonnazh = 70000;
+            goruchee = 0;
+            buttonSelectColor.BackColor = color;
+            buttonSelectDopColor.BackColor = dopColor;
+
+        }
+
+
+
+        private void buttonSelectColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                color = cd.Color;
+                buttonSelectColor.BackColor = color;
+            }
+        }
+
+        private void buttonSelectDopColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dopColor = cd.Color;
+                buttonSelectDopColor.BackColor = dopColor;
+            }
+        }
+        private void textBoxMaxSpeed_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMaxCountEkipazh_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool checkFields()
+        {
+            if (!int.TryParse(textBox1.Text, out maxSpeed))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBox2.Text, out maxCountEkipazh))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBox3.Text, out Tonnazh))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBox4.Text, out goruchee))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void buttonSetKorabl_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inter = new Ship(maxSpeed, maxCountEkipazh, Tonnazh, goruchee, color);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.draw(gr);
+                pictureBox1.Image = bmp;
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSetLinkor_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inter = new Kreiser(maxSpeed, maxCountEkipazh, goruchee, Tonnazh, color, checkBoxBrony.Checked, checkBoxZenit.Checked, dopColor);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.draw(gr);
+                pictureBox1.Image = bmp;
+            }
+
+        }
+
+        private void checkBoxBrony_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxZenit_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            if (inter != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.move(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+    }
+}
---
+++ 3laba/WindowsFormsApp3/WindowsFormsApp3/Form2.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxParking = new System.Windows.Forms.PictureBox();
+            this.buttonSetCar = new System.Windows.Forms.Button();
+            this.buttonSetGru = new System.Windows.Forms.Button();
+            this.buttonTakeCar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxTakeCar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxParking
+            // 
+            this.pictureBoxParking.Location = new System.Drawing.Point(2, 2);
+            this.pictureBoxParking.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxParking.Name = "pictureBoxParking";
+            this.pictureBoxParking.Size = new System.Drawing.Size(659, 404);
+            this.pictureBoxParking.TabIndex = 0;
+            this.pictureBoxParking.TabStop = false;
+            // 
+            // buttonSetCar
+            // 
+            this.buttonSetCar.Location = new System.Drawing.Point(681, 11);
+            this.buttonSetCar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSetCar.Name = "buttonSetCar";
+            this.buttonSetCar.Size = new System.Drawing.Size(138, 41);
+            this.buttonSetCar.TabIndex = 1;
+            this.buttonSetCar.Text = "Припарковать авто";
+            this.buttonSetCar.UseVisualStyleBackColor = true;
+            this.buttonSetCar.Click += new System.EventHandler(this.buttonSetCar_Click_1);
+            // 
+            // buttonSetGru
+            // 
+            this.buttonSetGru.Location = new System.Drawing.Point(681, 77);
+            this.buttonSetGru.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSetGru.Name = "buttonSetGru";
+            this.buttonSetGru.Size = new System.Drawing.Size(138, 40);
+            this.buttonSetGru.TabIndex = 2;
+            this.buttonSetGru.Text = "Припарковать грузовик";
+            this.buttonSetGru.UseVisualStyleBackColor = true;
+            this.buttonSetGru.Click += new System.EventHandler(this.buttonSetGru_Click);
+            // 
+            // buttonTakeCar
+            // 
+            this.buttonTakeCar.Location = new System.Drawing.Point(692, 245);
+            this.buttonTakeCar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTakeCar.Name = "buttonTakeCar";
+            this.buttonTakeCar.Size = new System.Drawing.Size(94, 31);
+            this.buttonTakeCar.TabIndex = 3;
+            this.buttonTakeCar.Text = "Забрать";
+            this.buttonTakeCar.UseVisualStyleBackColor = true;
+            this.buttonTakeCar.Click += new System.EventHandler(this.buttonTakeCar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(690, 164);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Забрать авто";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(760, 203);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.maskedTextBox1.Mask = "00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(76, 20);
+            this.maskedTextBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(692, 206);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Место";
+            // 
+            // pictureBoxTakeCar
+            // 
+            this.pictureBoxTakeCar.Location = new System.Drawing.Point(681, 281);
+            this.pictureBoxTakeCar.Name = "pictureBoxTakeCar";
+            this.pictureBoxTakeCar.Size = new System.Drawing.Size(241, 115);
+            this.pictureBoxTakeCar.TabIndex = 7;
+            this.pictureBoxTakeCar.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 408);
+            this.Controls.Add(this.pictureBoxParking);
+            this.Controls.Add(this.pictureBoxTakeCar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTakeCar);
+            this.Controls.Add(this.buttonSetGru);
+            this.Controls.Add(this.buttonSetCar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeCar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxParking;
+        private System.Windows.Forms.Button buttonSetCar;
+        private System.Windows.Forms.Button buttonSetGru;
+        private System.Windows.Forms.Button buttonTakeCar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBoxTakeCar;
+    }
+}
